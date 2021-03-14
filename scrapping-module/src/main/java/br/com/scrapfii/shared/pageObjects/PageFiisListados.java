7d7a1aeaf5582444cbd9b9e6888dab9415a7549b
Jonas Góes <jonasdossantos1991@gmail.com>
2021-03-13 22:47:48 -0300

    adicionados métodos de pesquisa e seleção de cards em exibição na classe FiisListados(TDD).

--- scrapping-module/src/main/java/br/com/scrapfii/shared/pageObjects/PageFiisListados.java
+++ scrapping-module/src/main/java/br/com/scrapfii/shared/pageObjects/PageFiisListados.java
@@ -1,11 +1,17 @@
 package br.com.scrapfii.shared.pageObjects;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import static org.openqa.selenium.By.tagName;
+import static org.openqa.selenium.By.xpath;
 
-import br.com.scrapfii.application.configuration.DefaultConfigurationDriver;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import br.com.scrapfii.application.configuration.DefaultWebDriverConfiguration;
 
 public class PageFiisListados extends PageObject{
 	
+	private static final String ID_SELECT_PAGE = "//*[@id=\"selectPage\"]";
 	private static final String MAIN_PAGE_URL = "https://sistemaswebb3-listados.b3.com.br/fundsPage/7";
 	private static final String SEARCH_INPUT = "//*[@id=\"palavrachave\"]";
 	private static final String SEARCH_BUTTON = "//*[@id=\"divContainerIframeB3\"]/form/div[1]/div/div/div[1]/div/div[2]/button";
@@ -13,18 +19,9 @@ public class PageFiisListados extends PageObject{
 	
 	private final WebDriver browser;
 	
-	public PageFiisListados(DefaultConfigurationDriver configs) {
+	public PageFiisListados(DefaultWebDriverConfiguration configs) {
 		super(configs);
 		this.browser = super.getDriver();
-	}
-
-	public FundDetail searchFor(String foundCode) {
-		this.goToMainPage();
-		this.browser.findElement(By.xpath(SEARCH_INPUT)).sendKeys(foundCode);
-		this.browser.findElement(By.xpath(SEARCH_BUTTON)).click();
-		this.browser.findElement(By.xpath(FIRST_CARD)).click();
-	
-		return new FundDetail(this.getConfigs());
 	}
 
 	@Override
@@ -32,7 +29,25 @@ public class PageFiisListados extends PageObject{
 		this.browser.navigate().to(MAIN_PAGE_URL);
 	}
 
+	public PageFundDetail searchFor(String foundCode) {
+		this.browser.findElement(xpath(SEARCH_INPUT)).sendKeys(foundCode);
+		this.browser.findElement(xpath(SEARCH_BUTTON)).click();
+		this.sleep(5);
+		this.browser.findElement(xpath(FIRST_CARD)).click();
+		return new PageFundDetail(this.getConfigs());
+	}
 
-
+	public WebElement nextPage() {
+		WebElement pagination = this.browser.findElement(xpath("//*[@id=\"listing_pagination\"]/pagination-template/ul"));
+		return pagination;
+	}
+	
+	public void selectTheLastOption() {
+	  List<WebElement>	options = this.browser.findElement(xpath(ID_SELECT_PAGE)).findElements(tagName("option"));
+	  if(!options.isEmpty()) {
+		  Integer size = options.size()-1;
+		  options.get(size).click();		  
+	  }
+	}
 
 }
