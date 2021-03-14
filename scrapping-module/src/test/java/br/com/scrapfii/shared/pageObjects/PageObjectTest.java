70d0659ca9e271a7184baf41042845bbaedb9466
Jonas Góes <jonasdossantos1991@gmail.com>
2021-03-13 22:51:03 -0300

    Criada a classe PageObjectTest, para todas as classes de testes herdarem configurações comuns.

---
+++ scrapping-module/src/test/java/br/com/scrapfii/shared/pageObjects/PageObjectTest.java
@@ -0,0 +1,29 @@
+package br.com.scrapfii.shared.pageObjects;
+
+import static java.util.Objects.isNull;
+
+import br.com.scrapfii.application.configuration.ChromeDriverConfiguration;
+import br.com.scrapfii.application.configuration.DefaultWebDriverConfiguration;
+
+public abstract class PageObjectTest {
+	
+	private static DefaultWebDriverConfiguration config;
+	
+	public static DefaultWebDriverConfiguration getWebDriverConfig() {
+		
+		if(isNull(config)) {
+			config = new ChromeDriverConfiguration();
+		}
+		
+		return config;
+	}
+	
+	
+	protected void sleep(Integer seconds){
+		
+		try { Thread.sleep(seconds*1000); } 
+		catch (InterruptedException exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+}
