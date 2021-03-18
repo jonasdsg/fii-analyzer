28c77b8d614fec693dd3af9db46764cc6d68f81c
Jonas Góes <jonasdossantos1991@gmail.com>
2021-03-17 22:45:11 -0300

    ajuste das classes entity

---
+++ scrapfii-application/src/main/java/br/com/scrapfii/application/fund/ContactEntity.java
@@ -0,0 +1,13 @@
+package br.com.scrapfii.application.fund;
+
+import br.com.scrapfii.domain.fund.Contact;
+
+public class ContactEntity extends Contact {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+
+
+	
+
+}
