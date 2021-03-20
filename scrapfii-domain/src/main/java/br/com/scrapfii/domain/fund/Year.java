a011df5d12bb8e98a2e5328470b56ea34d25def0
Jonas Góes <jonasdossantos1991@gmail.com>
2021-03-20 18:51:50 -0300

    ajuste para permitir a compilação

--- scrapfii-domain/src/main/java/br/com/scrapfii/domain/fund/Year.java
+++ scrapfii-domain/src/main/java/br/com/scrapfii/domain/fund/Year.java
@@ -12,7 +12,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 @Entity
 public class Year implements Serializable {
 	private static final long serialVersionUID = 1L;
